bf305a9ae45b716d8c76ebc51016859d4e03eaaf
justforc <ghemaallan@gmail.com>
2022-09-14 11:39:41 +0700

    fix: Udah bisa jadi hex cuma masih ada beberapa data yang ilang

--- models/echo_test_request.go
+++ models/echo_test_request.go
@@ -1,22 +1,18 @@
 package models
 
-import (
-	"github.com/moov-io/iso8583/field"
-)
+import "github.com/ideazxy/iso8583"
 
 type EchoTestRequestISO struct {
-	MTI                              *field.Numeric `index:"0"`
-	PrimaryBitMap                    *field.Bitmap  `index:"2"`
-	SecondaryBitMP                   *field.Bitmap  `index:"1"`
-	TransmisionDateAndTime           *field.Numeric `index:"7"`
-	SystemTraceAuditNumber           *field.Numeric `index:"11"`
-	SettlementDate                   *field.Numeric `index:"15"`
-	NetworkManagementInformationCode *field.Numeric `index:"70"`
+	SecondaryBitMap                  *iso8583.Alphanumeric `field:"1" length:"16" encode:"ascii"`
+	TransmissionDateAndTime          *iso8583.Numeric      `field:"7" length:"10" encode:"ascii"`
+	SystemTraceAuditNumber           *iso8583.Numeric      `field:"11" length:"6" encode:"ascii"`
+	AdditionalData                   *iso8583.Lllvar       `field:"48" length:"20" encode:"ascii"`
+	NetworkManagementInformationCode *iso8583.Numeric      `field:"70" length:"3" encode:"ascii"`
 }
 
 type EchoTestRequestJSON struct {
-	PrimaryBitMap          string `json:"primary_bit_map"`
-	SecondaryBitMP         string `json:"secondary_bit_map"`
+	SecondaryBitMap        string `json:"secondary_bit_map"`
 	SystemTraceAuditNumber string `json:"system_trace_audit_number"`
 	SettlementDate         string `json:"settlement_date"`
+	AdditionalData         string `json:"additional_data"`
 }
